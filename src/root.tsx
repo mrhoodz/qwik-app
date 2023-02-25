ead97cf9e0aa478bed2f9423f8aa0357adf95598
mrhoodz <hoodzt7@gmail.com>
2023-02-25 13:27:55 +0200

    February 25th, 2023.

--- src/root.tsx
+++ src/root.tsx
@@ -23,6 +23,17 @@ export default component$(() => {
       <head>
         <meta charSet="utf-8" />
         <link rel="manifest" href="/manifest.json" />
+        {/* 
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin="use-credentials"
+        />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700&display=swap"
+          rel="stylesheet"
+        /> */}
 
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link
@@ -31,7 +42,7 @@ export default component$(() => {
           crossOrigin="anonymous"
         />
         <link
-          href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700&display=swap"
+          href="https://fonts.googleapis.com/css2?family=Lato&family=Poppins:wght@400;500;600;700&display=swap"
           rel="stylesheet"
         />
 
