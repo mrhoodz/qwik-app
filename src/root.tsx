73eac67b18697a91f8b27cbdb899ce28eed5b23f
mrhoodz <hoodzt7@gmail.com>
2023-03-09 07:41:27 +0200

    28 february 2022

--- src/root.tsx
+++ src/root.tsx
@@ -31,16 +31,16 @@ export default component$(() => {
       <head>
         <link rel="icon" type="image/x-icon" href="/favicon.ico" />
 
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link
-          rel="preconnect"
-          href="https://fonts.gstatic.com"
-          crossOrigin="anonymous"
-        />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Lato&family=Poppins:wght@400;500;600;700&display=swap"
-          rel="stylesheet"
-        />
+        {/* <link rel="preconnect" href="https://fonts.googleapis.com" />
+      <link
+        rel="preconnect"
+        href="https://fonts.gstatic.com"
+        crossOrigin="anonymous"
+      />
+      <link
+        href="https://fonts.googleapis.com/css2?family=Lato&family=Poppins:wght@400;500;600;700&display=swap"
+        rel="stylesheet"
+      /> */}
 
         <meta charSet="utf-8" />
         {/* <!-- Google tag (gtag.js) --> */}
